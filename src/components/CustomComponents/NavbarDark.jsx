59e0b6af6d83e24af362e7a4fdfab7d873f1ecf6
tomideadeoye <tommideadeoye@gmail.com>
2022-04-12 18:27:04 +0100

    href

--- src/components/CustomComponents/NavbarDark.jsx
+++ src/components/CustomComponents/NavbarDark.jsx
@@ -1,12 +1,10 @@
 import MKBox from "components/MKBox";
 import DefaultNavbar from "examples/Navbars/DefaultNavbar";
-import { routes } from "routes";
 
 function NavbarDark() {
   return (
     <MKBox variant="gradient" bgColor="dark" shadow="sm" py={0.25}>
       <DefaultNavbar
-        routes={routes}
         action={{
           type: "external",
           href: "https://docs.google.com/spreadsheets/d/1X_oPC8_oaiQ-eLgWvGir8QkMvWZukYLS/edit?usp=sharing&ouid=109599550030499733240&rtpof=true&sd=true",
